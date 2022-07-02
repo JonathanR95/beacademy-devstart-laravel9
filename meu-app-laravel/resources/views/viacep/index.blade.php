bc59ada6909331c315de311d7ea580e17aa287d2
Jonathan Rodrigues <jonathan.rf.sousa@gmail.com>
2022-07-02 07:47:05 -0300

    primeiro commit

---
+++ meu-app-laravel/resources/views/viacep/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Via cep</title>
+</head>
+<body>
+    <div>
+        <form action="{{route ('viacep.index.post') }}" method="post">
+            @csrf
+            <label for="cep">Digite seu cep: </label>
+            <input type="text" name="cep">
+            <button type="submit">Enviar</button>     
+
+        </form>
+    </div>
+</body>
+</html>
